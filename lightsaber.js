661fba3e8253c26938fb83c8f45a73545baea730
sugarshin <shinsugar@gmail.com>
2014-12-03 02:04:52 +0900

    v1.0.0

--- lightsaber.js
+++ lightsaber.js
@@ -1,10 +1,11 @@
 /*!
- * @license lightsaber v0.0.2
+ * @license lightsaber v1.0.0
  * (c) 2014 sugarshin https://github.com/sugarshin
  * License: MIT
  */
 (function() {
-  var BufferLoader, Lightsaber;
+  var BufferLoader, Lightsaber,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   BufferLoader = (function() {
     function BufferLoader(context, urlList, callback) {
@@ -57,49 +58,118 @@
   })();
 
   Lightsaber = (function() {
-    var AudioContext;
+    var AudioContext, _isDeviceActivate;
 
     AudioContext = window.AudioContext || window.webkitAudioContext;
 
-    function Lightsaber(audioPath, startBtn) {
+    _isDeviceActivate = false;
+
+    function Lightsaber(arrayAudioPath, startBtn) {
+      this.shake = __bind(this.shake, this);
       this.startBtn = startBtn;
       this.context = new AudioContext;
-      this.bufferLoader = new BufferLoader(this.context, [audioPath]);
+      this.bufferLoader = new BufferLoader(this.context, arrayAudioPath);
       this.bufferLoader.load();
+      this.isPlaying = false;
+      this.isStart = false;
       this.events();
     }
 
-    Lightsaber.prototype.play = function(buffer, vol) {
-      var gainNode, source;
-      source = this.context.createBufferSource();
-      gainNode = this.context.createGain();
-      source.buffer = buffer;
-      source.connect(gainNode);
-      gainNode.connect(this.context.destination);
-      gainNode.gain.value = vol != null ? vol : 0;
-      return source.start(0);
+    Lightsaber.prototype.soundPlay = function(buffer, vol, loopSound) {
+      var loopGainNode, loopSource;
+      if (loopSound === true) {
+        loopSource = this.context.createBufferSource();
+        loopGainNode = this.context.createGain();
+        loopSource.buffer = buffer;
+        loopSource.connect(loopGainNode);
+        loopGainNode.connect(this.context.destination);
+        loopGainNode.gain.value = vol != null ? vol : 0;
+        loopSource.loop = true;
+        loopSource.start(0);
+      }
+      this.source = this.context.createBufferSource();
+      this.gainNode = this.context.createGain();
+      console.log(this.source);
+      this.source.buffer = buffer;
+      this.source.connect(this.gainNode);
+      this.gainNode.connect(this.context.destination);
+      this.gainNode.gain.value = vol != null ? vol : 0;
+      if (this.isStart === false) {
+        this.source.start(0);
+      }
+      if (this.isPlaying === false && this.isStart === true) {
+        this.source.start(0);
+        this.isPlaying = true;
+        return this.source.onended = (function(_this) {
+          return function(ev) {
+            return _this.isPlaying = false;
+          };
+        })(this);
+      }
+    };
+
+    Lightsaber.prototype.shake = function(event) {
+      var aig, _ref, _ref1, _ref2;
+      aig = event.accelerationIncludingGravity;
+      if ((20 > (_ref = aig.x) && _ref > 15) || (20 > (_ref1 = aig.y) && _ref1 > 15) || (20 > (_ref2 = aig.z) && _ref2 > 15)) {
+        return this.soundPlay(this.bufferLoader.bufferList[2], 1);
+      } else if (aig.x > 30 || aig.y > 30 || aig.z > 30) {
+        return this.soundPlay(this.bufferLoader.bufferList[3], 1);
+      }
     };
 
     Lightsaber.prototype.rm = function(el) {
       return el.parentNode.removeChild(el);
     };
 
+    Lightsaber.prototype.start = function() {
+      this.addMotionEvent();
+      this.soundPlay(this.bufferLoader.bufferList[0], 1);
+      this.isStart = true;
+      return document.getElementById('sword').style.display = 'block';
+    };
+
+    Lightsaber.prototype.end = function() {
+      this.rmMotionEvent();
+      this.soundPlay(this.bufferLoader.bufferList[4], 1);
+      this.isStart = false;
+      return document.getElementById('sword').style.display = 'none';
+    };
+
+    Lightsaber.prototype.toggle = function() {
+      if (this.isStart === false) {
+        return this.start();
+      } else {
+        return this.end();
+      }
+    };
+
     Lightsaber.prototype.events = function() {
-      window.addEventListener('devicemotion', (function(_this) {
-        return function(ev) {
-          var aig;
-          aig = ev.accelerationIncludingGravity;
-          if (aig.x > 20 || aig.y > 20 || aig.z > 20) {
-            return _this.play(_this.bufferLoader.bufferList[0], 1);
-          }
-        };
-      })(this));
       return this.startBtn.addEventListener('click', (function(_this) {
         return function() {
-          _this.play(_this.bufferLoader.bufferList[0]);
-          return _this.rm(_this.startBtn);
+          if (_isDeviceActivate === false) {
+            _this.soundPlay(_this.bufferLoader.bufferList[0]);
+            _isDeviceActivate = true;
+          }
+          return _this.toggle();
         };
       })(this));
+    };
+
+    Lightsaber.prototype.addEvent = function(el, type, eventHandler) {
+      return el.addEventListener(type, eventHandler);
+    };
+
+    Lightsaber.prototype.rmEvent = function(el, type, eventHandler) {
+      return el.removeEventListener(type, eventHandler);
+    };
+
+    Lightsaber.prototype.addMotionEvent = function() {
+      return this.addEvent(window, 'devicemotion', this.shake);
+    };
+
+    Lightsaber.prototype.rmMotionEvent = function() {
+      return this.rmEvent(window, 'devicemotion', this.shake);
     };
 
     return Lightsaber;
